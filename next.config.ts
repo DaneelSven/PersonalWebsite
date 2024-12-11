459530eef412111b24b384907f991a9d0acb706b
Sven Daneel <sven4696@gmail.com>
2024-12-11 15:41:21 +0100

    clean up for vercel

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
-};
-
-export default nextConfig;
