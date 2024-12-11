9acf35b9c63a8dffff4bf7bbf010fbe9a1d20e82
Sven Daneel <sven4696@gmail.com>
2024-12-11 20:40:16 +0100

    dev

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+const nextConfig = {
   /* config options here */
   transpilePackages: ["three", "@react-three/fiber", "@react-three/drei"],
+  reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
